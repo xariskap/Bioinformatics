38818161a5aa881f67b272043968266354f8c9ae
Xaris Kap <xariskapelet@gmail.com>
2023-05-27 22:58:50 +0300

    4_2

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-#get the max value of a 2d array
-def getMax(matrix):
-
-    row, column = np.unravel_index(matrix.argmax(), matrix.shape)
-    print(f'The max number is at index {row, column} and it is {matrix[row][column]}')
-    return row,column
