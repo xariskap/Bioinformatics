b700fb471a49f0a517991bbf97c9afe71a48b985
Yannis Kyriakopoulos <66531087+JohnnyKyr@users.noreply.github.com>
2023-06-24 14:15:23 +0300

    Update 4a.py

--- 4a.py
+++ 4a.py
@@ -1,13 +1,24 @@
-string = "nanakali"
+import numpy as np
+string  = "ababbasdfabbbbb"
 n = len(string)
-c = int((n-1)/2)
-for i in range(c):
-        #print(string[0],string[c+1],c+1)
-        if string[0] == string[c+1] and len(string[0:c+1])==len(string[c+1:c+2+c]):
-                print(string[0:c+1],string[c+1:c+2+c])
-                if string[:c+1]==string[c+1:c+2+c]:
-                        print(string[0:c+1])
-                        print("Success")
-                        break
-        else:
-                c -=1
+sc_mat = []
+j=0
+i=1
+sc_mat.append(j)
+while len(sc_mat)<len(string):
+  if string[i] == string[j]:
+    
+    j+=1
+    i+=1
+    sc_mat.append(j)
+  else:
+    if j==0:
+      sc_mat.append(0)
+      i+=1
+    if j!=0:
+      j = sc_mat[j-1]
+    
+i,m =0, getMax(sc_mat)
+c = int((m)/2)
+if string[:c+1] == string[c+1:m+1]:
+  print(string[:c+1], string[c+1:m+1])
