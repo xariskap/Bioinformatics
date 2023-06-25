309d25dc58e33bb33c6fef9b7d865e8ed4c0e090
Xaris Kap <78042499+xariskap@users.noreply.github.com>
2023-06-25 23:21:20 +0300

    Update ex4_1.py

--- ex4_1.py
+++ ex4_1.py
@@ -1,15 +1,39 @@
-string  = "abacabacskla"
-n = len(string)
-sc_mat = []
-j=0
-sc_mat.append(j)
-for i in range(1,n):
-  if string[i] == string[j]:
-    
-    j+=1
-    sc_mat.append(j)
-  else:
-    j=0
-    sc_mat.append(j)
-    
+import numpy as np
+
+def getMax(matrix):
+  arr = np.array(matrix)
+  return arr.argmax()
+
+
+string  = "banabanabna"
+def lps(string):
+  n = len(string)
+  sc_mat = []
+  j=0
+  i=1
+  sc_mat.append(j)
+  while len(sc_mat)<len(string):
+    if string[i] == string[j]:
+
+      j+=1
+      i+=1
+      sc_mat.append(j)
+    else:
+      if j==0:
+        sc_mat.append(0)
+        i+=1
+      if j!=0:
+        j = sc_mat[j-1]
+  return sc_mat
+
+
+sc_mat = lps(string)
+i,m =0, getMax(sc_mat)
+c = int((m-1)/2)
 print(sc_mat)
+if string[:c+1] == string[c+1:c+len(string[:c+1])+1]:
+  print("The longest tandem repeat is: ",end="")
+  print( string[c+1:c+len(string[:c+1])+1])
+else:
+  print("There is no tandem repeat")
+
